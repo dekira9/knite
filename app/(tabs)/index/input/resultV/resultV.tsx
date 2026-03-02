01dc563460cff2cbd94b75ffc71f9a623e5ee461
dekira9 <dekira9@gmail.com>
2026-03-02 22:36:19 +0300

    моя версияИра2марта

--- app/(tabs)/index/input/resultV/resultV.tsx
+++ app/(tabs)/index/input/resultV/resultV.tsx
@@ -22,7 +22,8 @@ import {
   calculateIncreaseRows1x2_1x3V,
   calculateIncreaseRows1x2_1x1V,
   calculateIncreaseRows1x4_1x3V,
-  determineIncreaseType
+  determineIncreaseType,
+  calculateVNeckIncreases23
 } from './helpers';
 import Step1RibbingV from './Step1RibbingV';
 import Step2AddingStitchesV from './Step2AddingStitchesV';
@@ -87,6 +88,7 @@ export default observer(() => {
   const { resultStringV22 } = calculateVNeckIncreases22(NRrezV, RowPribRV2, SpribVcorn);
   const { resultStringV11 } = calculateVNeckIncreases11(NRrezV, RowPribRV1, SpribVcorn);
   const { resultStringV12 } = calculateVNeckIncreases12(NRrezV, RowPribRV1, RowPribRV2);
+  const { resultStringV23 } = calculateVNeckIncreases23(NRrezV, RowPribRV2, RowPribRV2);
   // Исправляем вызов функции - убираем лишний параметр SVfront
   const { resultStringV01 } = calculateVNeckIncreases01(NRrezV, SpribVcorn, RowPribRV1, RowPribRVz);
   console.log('resultStringV11', resultStringV11);
@@ -220,6 +222,7 @@ export default observer(() => {
           resultStringV11={resultStringV11}
           resultStringV12={resultStringV12}
           resultStringV22={resultStringV22}
+          resultStringV23={resultStringV23}
         />
         
         <Step2AddingStitchesV 
